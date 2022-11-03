2c4cc4971ef68eb88ccd4e10a5251a66e3e33a36
Valentine <mywin@live.ru>
2022-11-03 20:49:00 +0300

    finish

--- 00/ex01/Contact.cpp
+++ 00/ex01/Contact.cpp
@@ -38,58 +38,58 @@ std::string Contact::getsecret() const {
 
 void Contact::addfname() {
 	std::cout<< "Insert your first name: ";
-	getline(std::cin, fname);
+	std::cin>> fname;
 	while (fname.length() >= 0) {
 		if (fname.empty())
 			std::cout<< "String cannot be empty. ";
 		else if (this->isstralpha(fname))
 			break;
 		std::cout<< "Enter only letters. Try again: ";
-		getline(std::cin, fname);
+		std::cin>> fname;
 	}
 }
 
 void Contact::addlname() {
 	std::cout<< "Insert your last name: ";
-	getline(std::cin, lname);
+	std::cin>> lname;
 	while (lname.length() >= 0) {
 		if (lname.empty())
 			std::cout<< "String cannot be empty. ";
 		else if (this->isstralpha(lname))
 			break;
 		std::cout<< "Enter only letters. Try again: ";
-		getline(std::cin, lname);
+		std::cin>> lname;
 	}
 }
 
 void Contact::addnickname() {
 	std::cout<< "Insert your nickname: ";
-	getline(std::cin, nickname);
+	std::cin>> nickname;
 	while (nickname.empty()) {
 		std::cout<< "String cannot be empty. Try again: ";
-		getline(std::cin, nickname);
+		std::cin>> nickname;
 	}
 }
 
 void Contact::addphonenumber() {
 	std::cout<< "Enter your phonenumber: ";
-	getline(std::cin, phonenumber);
+	std::cin>> phonenumber;
 	while ((phonenumber.length()) >= 0) {
 		if ((phonenumber.length() != 7) && (phonenumber.length() != 10))
 			std::cout<< "Phonenumber must contain 7 or 10 digits, without 8(+7). ";
 		else if (this->isstrdigit(phonenumber)) {
 			break;
 		}
-		std::cout<< "Enter only digits. Try again" <<std::endl;
-		getline(std::cin, phonenumber);
+		std::cout<< "Enter only digits. Try again: "<<std::endl;
+		std::cin>> phonenumber;
 	}
 }
 
 void Contact::addsecret() {
 	std::cout<< "Please, share your dark secret: ";
-	getline(std::cin, darkestsecret);
+	std::cin>> darkestsecret;
 	while (darkestsecret.empty()) {
 		std::cout<< "String cannot be empty. Try again: ";
-		getline(std::cin, darkestsecret);
+		std::cin>> darkestsecret;
 	}
 }
